9c31d8ad5643a44c04a6decd7892deb7b87b572f
eekelof <erik.ekelof1@gmail.com>
2024-10-03 14:27:39 +0200

    added support for components

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 const svgs = new Set(['svg', 'circle', 'ellipse', 'line', 'path', 'polygon', 'polyline', 'rect', 'g', 'defs', 'linearGradient', 'radialGradient', 'stop', 'use', 'symbol', 'text', 'tspan', 'tref', 'textPath', 'altGlyph', 'altGlyphDef', 'altGlyphItem', 'glyph', 'glyphRef', 'marker', 'metadata', 'missing-glyph', 'pattern', 'switch', 'foreignObject', 'desc', 'title'])
 export default function jsx(type, props, ...children) {
+    if (typeof type === 'function') return type(props, children)
     let e = svgs.has(type) ? document.createElementNS('http://www.w3.org/2000/svg', type) : document.createElement(type)
     for (let [k, v] of Object.entries((props ?? {})))
         k.startsWith('on') && typeof v === 'function' ? e.addEventListener(k.toLowerCase().substring(2), v)
